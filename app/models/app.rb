b3941d95e567c3162e234e5e233b43977d70ec23
Michael Huynh <mike.huynh@gmail.com>
2013-10-17 11:11:39 -0400

    Added placeholder for App model after_save and before_delete

--- app/models/app.rb
+++ app/models/app.rb
@@ -12,9 +12,18 @@
 require 'resolv'
 
 class App < ActiveRecord::Base
+  after_save :update_dns
+  before_destroy :remove_dns
+
   validates :name, :presence => true,
                    :length => { :maximum => 255 }
   #http://stackoverflow.com/questions/3756184/rails-3-validate-ip-string
   validates :address, :presence => true, 
                       :format => { :with => Resolv::IPv4::Regex }
+
+  def update_dns
+  end
+
+  def remove_dns
+  end
 end
