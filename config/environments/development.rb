b67192189b92d67443668fd34262ff3b2a519257
Michael Huynh <mike.huynh@gmail.com>
2013-10-16 22:41:15 -0400

    rails_admin install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ DokkuappCom::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # For rails_admin
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
