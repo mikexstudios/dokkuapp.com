e5273952adb8a89595726d7da84fa6080fa52a96
Michael Huynh <mike.huynh@gmail.com>
2013-10-16 23:36:46 -0400

    Added validation on App.name and App.address.

--- app/models/app.rb
+++ app/models/app.rb
@@ -9,5 +9,12 @@
 #  updated_at :datetime
 #
 
+require 'resolv'
+
 class App < ActiveRecord::Base
+  validates :name, :presence => true,
+                   :length => { :maximum => 255 }
+  #http://stackoverflow.com/questions/3756184/rails-3-validate-ip-string
+  validates :address, :presence => true, 
+                      :format => { :with => Resolv::IPv4::Regex }
 end
