0a2ebe85a99a00a904d3c8300829f0ae7f0ee120
Michael Huynh <mike.huynh@gmail.com>
2013-10-17 22:50:54 -0400

    POST to /api/v1/apps now creates :name and :address association.

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,3 +1,5 @@
+require 'resolv' #for validating IP address
+
 class API < Grape::API
   prefix 'api'
   version 'v1', using: :path
@@ -15,27 +17,30 @@ class API < Grape::API
 
     desc "Return a app's address given the app's name."
     params do
-      requires :id, type: String, desc: "App name."
+      requires :name, type: String, desc: "Name of app."
     end
-    route_param :id do
+    route_param :name do
       get do
-        a = App.find_by name: params[:id]
+        a = App.find_by! name: params[:name]
         return {name: a.name,
                 address: a.address}
       end
     end
 
-    #desc "Create an app association."
-    #params do
-    #  requires :name, type: String, desc: "Name of app."
-    #  requires :address, type: String, desc: "Address of app."
-    #end
-    #post do
-    #  App.create!({
-    #    name: params[:name],
-    #    address: params[:address]
-    #  })
-    #end
+    desc "Create an association between an app name and server address."
+    params do
+      requires :name, type: String, desc: "Name of app."
+      requires :address, type: String, regexp: Resolv::IPv4::Regex, 
+                         desc: "Address of app server."
+    end
+    post do
+      a = App.find_or_initialize_by name: params[:name]
+      a.address = params[:address]
+      a.save
+      return {name: a.name,
+              address: a.address}
+    end
+
 
     #desc "Delete an app."
     #params do
