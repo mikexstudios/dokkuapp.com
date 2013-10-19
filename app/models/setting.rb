0538998866ed8bf5a4714d76e89b3f48dcccfadf
Michael Huynh <mike.huynh@gmail.com>
2013-10-19 15:33:04 -0400

    Used generator to create settings and migration.

---
+++ app/models/setting.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: settings
+#
+#  id         :integer          not null, primary key
+#  var        :string(255)      not null
+#  value      :text
+#  thing_id   :integer
+#  thing_type :string(30)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Setting < RailsSettings::CachedSettings
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018172657) do
+ActiveRecord::Schema.define(version: 20131019193105) do
 
   create_table "apps", force: true do |t|
     t.string   "name"
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 20131018172657) do
   end
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories"
+
+  create_table "settings", force: true do |t|
+    t.string   "var",                   null: false
+    t.text     "value"
+    t.integer  "thing_id"
+    t.string   "thing_type", limit: 30
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "settings", ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
